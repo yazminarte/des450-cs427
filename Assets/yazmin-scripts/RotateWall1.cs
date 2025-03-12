e0d80b70070219e59bcd68f3e1a911408948fe99
yazminarte <yarte2@uic.edu>
2025-03-12 02:12:05 -0500

    interaction and room update

---
+++ Assets/yazmin-scripts/RotateWall1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateWall1 : MonoBehaviour
+{
+
+    public float speed = 15;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+       transform.Rotate(Vector3.up, 15 * Time.deltaTime);
+    
+    }
+}
---
+++ Assets/yazmin-scripts/rainddropvars.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vars : MonoBehaviour
+{
+
+    int myInt = 5;
+    // Start is called before the first frame update
+    void Start()
+    {int myInt = 55;
+        Debug.Log(myInt*2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

ef1b904e21dd45c71254028a2bb2495c3bdab7ef
Jade Junghoo Lee <jlee911@uic.edu>
2025-03-17 18:22:42 -0500

    sound-effects

--- Assets/Jade_scripts/ClickToBounceBall.cs
+++ Assets/Jade_scripts/ClickToBounceBall.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class ClickToBounceBall : MonoBehaviour
 {
     private Rigidbody rb;
     private bool isFalling = false;
+    private AudioSource audioSource;
+    public AudioClip clickSound;
+    public AudioClip bounceSound;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.useGravity = false;
         rb.constraints = RigidbodyConstraints.FreezePositionY;
+
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -27,6 +31,11 @@ public class ClickToBounceBall : MonoBehaviour
                 if (hit.collider.gameObject == gameObject)
                 {
                     ActivateGravity();
+
+                    if (audioSource != null && clickSound != null)
+                    {
+                        audioSource.PlayOneShot(clickSound);
+                    }
                 }
             }
         }
@@ -48,6 +57,11 @@ public class ClickToBounceBall : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             rb.velocity = new Vector3(rb.velocity.x, Mathf.Abs(rb.velocity.y) * 0.9f, rb.velocity.z);
+
+            if (audioSource != null && bounceSound != null)
+            {
+                audioSource.PlayOneShot(bounceSound);
+            }
         }
     }
 }
